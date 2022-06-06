26d8ed8570abe10439b2922e44bf09cfbd018827
MHeinz1996 <michael.heinzinger@gmail.com>
2022-06-06 13:31:39 -0500

    Completed algo

--- js/pigLatin.js
+++ js/pigLatin.js
@@ -1,2 +1,29 @@
 exports.translate = function(word) {
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    pigLatin = []
+
+    splitWords = word.split(' ');
+    count = 0
+    for(i=0; i<splitWords.length; i++) {
+        for(j=0; j<splitWords[i].length; j++) {
+            if(vowels.includes(splitWords[i][j]) == false) {
+                if(splitWords[i][j] == 'q' && splitWords[i][j+1] == 'u') {
+                    splitWords[i] += (splitWords[i][j] + splitWords[i][j+1])
+                    count+=2
+                } else {
+                    splitWords[i] += splitWords[i][j];
+                    count++
+                }
+            } else {
+                break;
+            }
+        }
+        
+        temp = splitWords[i].substring(count) + 'ay'
+        pigLatin.push(temp)
+        count = 0
+    }
+    return pigLatin.join(' ')
 };
+
+// console.log(exports.translate('quiet'))
